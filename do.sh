b98fe3739054c2f0b09f31bf108a9e152980b1e8
thomaskost17 <thomaskost17@ucla.edu>
2022-03-23 00:17:19 -0700

    fine tuned shell script

--- do.sh
+++ do.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
-while getopts d: flag
-do
-    case "${flag}" in
-        d) get_data=true;;
+get_data=false
+while getopts ":d" flag;do
+    case $flag in
+        d) 
+		get_data=true
+		;;
+	\?) 
+		echo "Invalid option: -$OPTARG"
+		;;
     esac
 done
 
 if $get_data; then
-	data_dir = "./data"
-	if [-d "data_dir"]; then
-
-		echo "Getting Data...";
-	else
-		
-		echo "Getting Data...";
-		mkdir data;
+	data_dir="./data"
+	if [ ! -d "$data_dir" ]
+       	then
+		mkdir data
 	fi
-
+	echo "Getting Data..."
         for i in 2015 2016 2017 2018 2019 2021
 	do
-		curl -# -o ./data/$i.csv "https://barttorvik.com/$(i)_team_results.csv";
+		curl -s -o ./data/"$i"_team.csv "https://barttorvik.com/"$i"_team_results.csv";
 	done
+	echo "Data Collected!"
 fi
+
+
